fe148838ea6a54ce667331ce2f9051d9573577b5
Shubham926 <bhandarishubham926@gmail.com>
2026-07-30 01:10:33 +0530

    flask backend completed

--- app/agents/graph.py
+++ app/agents/graph.py
@@ -6,3 +6,40 @@ from app.agents.nodes.retriever import retrieve_node
 from app.agents.nodes.responder import generate_node
 
 
+workflow=StateGraph(AgentState)
+
+
+workflow.add_node("planner", planner_node)
+workflow.add_node("retriever",retrieve_node)
+workflow.add_node("responder",generate_node)
+
+
+def route_planner(state:AgentState):
+    """
+    Routes the workflow basef on the planner's decision.
+    """
+
+    if state["current_query"]=="CONVERSATIONAL":
+        return "responder"
+    return "retriever"
+
+
+workflow.set_entry_point("planner")
+
+
+workflow.add_conditional_edges(
+    "planner",
+    route_planner,
+    {
+        "retriever":"retriever",
+        "responder":"responder"
+    }
+)
+
+
+workflow.add_edge("retriever","responder")
+workflow.add_edge("responder",END)
+
+checkpointer=MemorySaver()
+
+rag_agent=workflow.compile(checkpointer=checkpointer)
